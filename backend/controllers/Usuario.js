bc07dc437682f1c35742091a6c0e625ba024a2fe
Valentin Albornoz <valeg486@gmail.com>
2023-10-07 23:06:43 -0300

    Revert "primer commit"
    This reverts commit 6ffd99495c839abce7f5a1b160a09b6c2be34219.

--- backend/controllers/Usuario.js
+++
@@ -1,7 +0,0 @@
-import Usuario from "../models/UsuarioModel.js";
-
-export const getUsuario = (req, res) => {};
-export const getUsuarioById = async (req, res) => {};
-export const createUsuario = async (req, res) => {};
-export const updateUsuario = async (req, res) => {};
-export const deleteUsuario = async (req, res) => {};
--- client/src/reducer/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../Auth/authSlice.js";
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
