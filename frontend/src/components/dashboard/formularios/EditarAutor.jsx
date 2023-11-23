6fe4f95d5972fd271a61119a2ad9702e161e6b96
Valentin Albornoz <valeg486@gmail.com>
2023-11-23 12:07:32 -0300

    edición

--- frontend/src/components/dashboard/formularios/EditarAutor.jsx
+++ frontend/src/components/dashboard/formularios/EditarAutor.jsx
@@ -1,71 +1,114 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useParams } from "react-router-dom";
 import Notification from "../../../components/notificacion/Notificacion";
-import { autorPorId } from "../../../service/autor/Listar.js";
-import { imagenPorIdAutor } from "../../../service/imagen/Imagen.js";
+import { listaAutores } from "../../../service/autor/Listar.js";
+import { listaSecciones } from "../../../service/seccion/Listar.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileImage } from "@fortawesome/free-solid-svg-icons";
+import fetch from "node-fetch";
+import { noticiaPorId } from "../../../service/noticia/Principales.js";
 
-function EditarAutor() {
-  const { id } = useParams();
-  const [autor, setAutor] = useState({});
-  const [foto, setFoto] = useState();
+const EditarNoticia = ({ id }) => {
   const {
     register,
     handleSubmit,
     setValue,
     formState: { errors },
   } = useForm();
-  const [fotoName, setFotoName] = useState("Subir foto");
-  const dataRef = useRef(null);
+
+  const [titulo, setTitulo] = useState("");
+  const [subtitulo, setSubtitulo] = useState("");
+  const [parrafos, setParrafos] = useState([""]);
+  const [etiquetas, setEtiquetas] = useState([""]);
+  const [idSeccion, setIdSeccion] = useState("");
+  const [idAutor, setIdAutor] = useState("");
+  const [portada, setPortada] = useState(null);
+  const [imagenes, setImagenes] = useState([]);
+  const [autores, setAutores] = useState([]);
+  const [secciones, setSecciones] = useState([]);
   const [showNotification, setShowNotification] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState("");
 
-  useEffect(() => {
-    imagenPorIdAutor(id)
-      .then((data) => {
-        setFoto(data);
-      })
-      .catch((error) =>
-        console.error("Error al obtener la imagen de usuario: ", error)
-      );
-  }, [id]);
+  const handleNuevoParrafoChange = (e, index) => {
+    const updatedParrafos = [...parrafos];
+    updatedParrafos[index] = e.target.value;
+    setParrafos(updatedParrafos);
+  };
 
-  useEffect(() => {
-    autorPorId(id)
-      .then((data) => {
-        setAutor(data);
-        // Al recibir los datos del autor, actualiza los campos de entrada
-        setValue("nombre", data.nombre);
-        setValue("apellido", data.apellido);
-      })
-      .catch((error) => console.error("Error al obtener el autor: ", error));
-  }, [id, setValue]);
+  const handleAgregarParrafo = () => {
+    setParrafos([...parrafos, ""]);
+  };
+
+  const handleNuevaEtiquetaChange = (e, index) => {
+    const updatedEtiquetas = [...etiquetas];
+    updatedEtiquetas[index] = e.target.value;
+    setEtiquetas(updatedEtiquetas);
+  };
+
+  const handleAgregarEtiqueta = () => {
+    setEtiquetas([...etiquetas, ""]);
+  };
+
+  const handleEliminarParrafo = (index) => {
+    const updatedParrafos = [...parrafos];
+    updatedParrafos.splice(index, 1);
+    setParrafos(updatedParrafos);
+  };
+
+  const handleEliminarEtiqueta = (index) => {
+    const updatedEtiquetas = [...etiquetas];
+    updatedEtiquetas.splice(index, 1);
+    setEtiquetas(updatedEtiquetas);
+  };
+
+  const handlePortadaChange = (e) => {
+    try {
+      setPortada(e.target.files[0]);
+    } catch (error) {
+      console.error("Error al actualizar la portada:", error);
+    }
+  };
+
+  const handleImagenesChange = (e) => {
+    try {
+      const newImages = [...imagenes];
+      newImages.push(e.target.files[0]);
+      setImagenes(newImages);
+    } catch (error) {
+      console.error("Error al actualizar las imágenes:", error);
+    }
+  };
+
+  const handleSeccionChange = (e) => {
+    setIdSeccion(e.target.value);
+  };
+
+  const handleAutorChange = (e) => {
+    setIdAutor(e.target.value);
+  };
 
   const handleNotificationClose = () => {
     setShowNotification(false);
     setNotificationMessage("");
   };
 
-  useEffect(() => {
-    if (errors.foto) {
-      setFotoName(errors.foto.message);
-    } else {
-      const foto = dataRef.current ? dataRef.current.foto : null;
-      setFotoName(foto ? foto[0].name : "Subir foto");
-    }
-  }, [errors.foto, dataRef]);
-
   const onSubmit = async (data) => {
-    dataRef.current = { ...data };
     const formData = new FormData();
-    formData.append("apellido", data.apellido);
-    formData.append("nombre", data.nombre);
-    formData.append("foto", data.foto[0]);
+    formData.append("titulo", data.titulo);
+    formData.append("subtitulo", data.subtitulo);
+    formData.append("idSeccion", idSeccion); // Using idSeccion
+    formData.append("idAutor", idAutor); // Using idAutor
+    formData.append("portada", portada);
+    imagenes.forEach((imagen, index) => {
+      formData.append("imagenes", imagen);
+    });
+    formData.append("parrafos", JSON.stringify(parrafos));
+    formData.append("etiquetas", JSON.stringify(etiquetas));
 
     try {
       const response = await fetch(
-        `http://localhost:8080/api/autor/editar/${id}`,
+        `http://localhost:8080/api/noticia/editar/${id}`,
         {
           method: "POST",
           body: formData,
@@ -73,8 +116,8 @@ function EditarAutor() {
       );
 
       if (response.ok) {
-        const responseData = await response.text();
-        setNotificationMessage(responseData);
+        const data = await response.text();
+        setNotificationMessage(data);
         setShowNotification(true);
       } else {
         setNotificationMessage(
@@ -89,83 +132,219 @@ function EditarAutor() {
     }
   };
 
+  useEffect(() => {
+    listaAutores()
+      .then((data) => setAutores(data))
+      .catch((error) =>
+        console.error("Error al obtener la lista de autores: ", error)
+      );
+
+    listaSecciones()
+      .then((data) => setSecciones(data))
+      .catch((error) =>
+        console.error("Error al obtener la lista de secciones: ", error)
+      );
+
+    noticiaPorId(id)
+      .then((noticia) => {
+        setTitulo(noticia.titulo);
+        setSubtitulo(noticia.subtitulo);
+        setIdSeccion(noticia.idSeccion);
+        setIdAutor(noticia.idAutor);
+        setPortada(noticia.portada);
+        setImagenes(noticia.imagenes);
+        setParrafos(noticia.parrafos);
+        setEtiquetas(noticia.etiquetas);
+      })
+      .catch((error) =>
+        console.error("Error al obtener la noticia para editar: ", error)
+      );
+  }, [id, setValue]);
+
   return (
     <section className="flexCT">
       <div className="form-container">
         <form onSubmit={handleSubmit(onSubmit)} className="form">
-          <span className="titulo">Editar Autor</span>
-          {foto && <img src={foto} alt="Foto" />}
+          <span className="titulo">Editar Noticia</span>
+
           <div>
-            <label htmlFor="nombre">Nombre</label>
+            <label htmlFor="titulo">Título</label>
             <input
               type="text"
-              id="nombre"
-              {...register("nombre", {
+              id="titulo"
+              {...register("titulo", {
                 required: "Por favor, completa este campo.",
               })}
               className="input"
-              defaultValue={(_) => autor.nombre} // Establece el valor inicial
+              value={titulo}
+              onChange={(e) => setTitulo(e.target.value)}
             />
-            {errors.nombre && (
-              <span className="error-msg">{errors.nombre.message}</span>
+            {errors.titulo && (
+              <span className="error-msg">{errors.titulo.message}</span>
             )}
           </div>
+
           <div>
-            <label htmlFor="apellido">Apellido</label>
+            <label htmlFor="subtitulo">Subtítulo</label>
             <input
               type="text"
-              id="apellido"
-              {...register("apellido", {
+              id="subtitulo"
+              {...register("subtitulo", {
                 required: "Por favor, completa este campo.",
               })}
               className="input"
-              defaultValue={(_) => autor.apellido} // Establece el valor inicial
+              value={subtitulo}
+              onChange={(e) => setSubtitulo(e.target.value)}
             />
-            {errors.apellido && (
-              <span className="error-msg">{errors.apellido.message}</span>
+            {errors.subtitulo && (
+              <span className="error-msg">{errors.subtitulo.message}</span>
             )}
           </div>
+
           <div>
-            <label className="cargar-archivo input" htmlFor="foto">
+            <label className="cargar-archivo input" htmlFor="portada">
               <div className="icono">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill=""
-                  viewBox="0 0 24 24"
-                >
-                  <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    id="SVGRepo_tracerCarrier"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <path
-                      fill=""
-                      d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
-                      clipRule="evenodd"
-                      fillRule="evenodd"
-                    ></path>
-                  </g>
-                </svg>
+                <FontAwesomeIcon icon={faFileImage} />
               </div>
               <div className="texto">
-                <span className={errors.foto ? "error-msg" : ""}>
-                  {errors.foto ? errors.foto.message : fotoName}
-                </span>
+                <span>Subir portada</span>
               </div>
-              <input
-                type="file"
-                id="foto"
-                {...register("foto", {
-                  required: "Por favor, sube una imagen.",
-                })}
-              />
             </label>
+            <input type="file" id="portada" onChange={handlePortadaChange} />
+
+            {errors.portada && (
+              <span className="error-msg">{errors.portada.message}</span>
+            )}
           </div>
+
+          <div>
+            <label className="cargar-archivo input" htmlFor="imagenes">
+              <div className="icono">
+                <FontAwesomeIcon icon={faFileImage} />
+              </div>
+              <div className="texto">
+                <span>Imagenes extra</span>
+              </div>
+            </label>
+            <input
+              type="file"
+              id="imagenes"
+              onChange={handleImagenesChange}
+              multiple
+            />
+            {errors.imagenes && (
+              <span className="error-msg">{errors.imagenes.message}</span>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="seccion">Seleccionar Sección:</label>
+            <select
+              id="seccion"
+              {...register("seccion", {
+                required: "Por favor, selecciona una sección.",
+              })}
+              value={idSeccion}
+              onChange={handleSeccionChange}
+              className="input"
+            >
+              <option value="">Seleccionar Sección</option>
+              {secciones.map((seccion) => (
+                <option key={seccion.id} value={seccion.id}>
+                  {seccion.nombre}
+                </option>
+              ))}
+            </select>
+            {errors.seccion && (
+              <span className="error-msg">{errors.seccion.message}</span>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="autor">Seleccionar Autor:</label>
+            <select
+              id="autor"
+              {...register("autor", {
+                required: "Por favor, selecciona un autor.",
+              })}
+              value={idAutor}
+              onChange={handleAutorChange}
+              className="input"
+            >
+              <option value="">Seleccionar Autor</option>
+              {autores.map((autor) => (
+                <option key={autor.id} value={autor.id}>
+                  {autor.nombre}
+                </option>
+              ))}
+            </select>
+            {errors.autor && (
+              <span className="error-msg">{errors.autor.message}</span>
+            )}
+          </div>
+
+          <div>
+            <label>Parrafos:</label>
+            {parrafos.map((parrafo, index) => (
+              <div key={index}>
+                <textarea
+                  value={parrafo}
+                  onChange={(e) => handleNuevoParrafoChange(e, index)}
+                  placeholder="Ingrese un párrafo"
+                  className="textarea"
+                  id="parrafo"
+                />
+                <button
+                  type="button"
+                  className="eliminar-parrafo-button"
+                  onClick={() => handleEliminarParrafo(index)}
+                >
+                  Eliminar Párrafo
+                </button>
+              </div>
+            ))}
+            <button
+              type="button"
+              className="agregar-parrafo-button"
+              onClick={handleAgregarParrafo}
+            >
+              Agregar Párrafo
+            </button>
+          </div>
+
+          <div>
+            <label>Etiquetas:</label>
+            {etiquetas.map((etiqueta, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={etiqueta}
+                  onChange={(e) => handleNuevaEtiquetaChange(e, index)}
+                  placeholder="Ingrese una etiqueta"
+                  className="textarea"
+                  id="etiqueta"
+                />
+                <button
+                  type="button"
+                  className="eliminar-etiqueta-button"
+                  onClick={() => handleEliminarEtiqueta(index)}
+                >
+                  Eliminar Etiqueta
+                </button>
+              </div>
+            ))}
+            <button
+              type="button"
+              className="agregar-etiqueta-button"
+              onClick={handleAgregarEtiqueta}
+            >
+              Agregar Etiqueta
+            </button>
+          </div>
+
           <div className="button-container">
             <button type="submit" className="send-button">
-              Editar Autor
+              Guardar Noticia
             </button>
             <div className="reset-button-container">
               <button id="reset-btn" className="reset-button" type="reset">
@@ -183,6 +362,6 @@ function EditarAutor() {
       )}
     </section>
   );
-}
+};
 
-export default EditarAutor;
+export default EditarNoticia;
