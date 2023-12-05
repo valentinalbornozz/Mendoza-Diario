97ae7158522b26783a453a2793e20b54ded08c65
Valentin Albornoz <103218160+valentinalbornozz@users.noreply.github.com>
2023-12-05 18:37:15 -0300

    Delete backend directory

--- backend/src/main/java/com/mendozanews/apinews/enums/Rol.java
+++
@@ -1,5 +0,0 @@
-package com.mendozanews.apinews.enums;
-
-public enum Rol {
-    USUARIO, ADMIN;
-}
--- backend/src/main/java/com/mendozanews/apinews/excepciones/MiException.java
+++
@@ -1,9 +0,0 @@
-
-package com.mendozanews.apinews.excepciones;
-
-public class MiException extends Exception {
-
-    public MiException(String msj) {
-        super(msj);
-    }
-}
